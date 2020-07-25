d900d5bd6c6c75fdbae884d3fa7864c1bd0e7d4f
mohammad-gh75 <ar.arsham@yahoo.com>
2020-07-25 13:30:04 +0430

    Initial commit create 2 activity and 2 fragment

---
+++ app/src/main/java/org/maktab36/taskapp/controller/activity/TaskListActivity.java
@@ -0,0 +1,16 @@
+package org.maktab36.taskapp.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.maktab36.taskapp.R;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
